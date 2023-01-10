450c775a91d6f3c76defbc1eb1bb96971baaa5c0
ehbenahmed <elhabib.benahmed@gmail.com>
2023-01-10 09:05:19 +0100

    tt

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React, {Component} from "react";
-
+import ContactComponent from './ContactComponent'
 import './App.css';
+import Navbar from "./Navbar";
 
 
 class App extends Component {
-    render() {
-        let name = "ahmad ben ahmad";
-        let num1 = 12;
-       let num2 = 120;
-        return (
-            <div className="App">
-                <h1>salam mes chers developpeurs</h1>
-                <label htmlFor="name">{name}</label>
-                <input type="number" name=""id=""/>
-                <p>{num1}+{num2}={num1+num2}</p>
-            </div>
-        )
-    }
+  render() {
+    let name = "ahmad ben ahmad";
+    let num1 = 12;
+    let num2 = 120;
+    return (
+        <div className="App">
+          <h1>salam mes chersdevloppeurs</h1>
+          <input type="text" name="" id=""/>
+            <h2>ahmad ben ahmad</h2>
+            <p>{num1}+{num2}={num1 + num2}</p>
+            <ContactComponent/>
+            <ContactComponent/>
+            <ContactComponent/>
+            <Navbar/>
+        </div>
 
-}
 
-
-export default App;
+  export default App;
--- src/ContactComponent.tsx
+++ src/ContactComponent.tsx
@@ -2,14 +2,17 @@ import React, {Component} from "react";
 
 
 class ContactComponent extends Component {
-    render() {
-        return (
-            <div>
-                <h1>contacts:mohamad IDBRAHIM </h1>
-
-            </div>
-        )
-    }
+  render() {
+    return (
+        <div>
+          <h1>contacts:mohamad IDBRAHIM </h1>
+          <ul>
+            <li>tel:0654631985</li>
+            <li>ahmad@gmail.com </li>
+          </ul>
+        </div>
+    )
+  }
 }
 
 export default ContactComponent;
